8352adc3a2f9e2f00ed1aa001d377f305b8dbd74
Rksoni-p <96594551+Rksoni-p@users.noreply.github.com>
2022-03-31 19:38:05 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -13,19 +13,19 @@ async def start_(client: Client, message: Message):
         photo=f"https://te.legra.ph/file/86aa550f64231113c8162.jpg",
         caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [SWEAT❤️](https://t.me/classy_network)
 
-𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [✨ 𝐊𝐈𝐍𝐆💜](https://t.me/s_e_l_f_i_s_h_x_7)
-𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :- [✨ 𝐐𝐔𝐄𝐄𝐍 ❤️🎸](https://t.me/complicatedgirl1)
-𝐃𝐢𝐬𝐜𝐮𝐬𝐬 :- [✨ 𝐆𝐑𝐎𝐔𝐏 🎧](https://t.me/secretgroupc)
-𝐒𝐨𝐮𝐫𝐜𝐞  :- [✨ 𝐍𝐄𝐓𝐖𝐎𝐑𝐊 🌍](https://t.me/classy_network)
-𝐂𝐨𝐦𝐦𝐚𝐧𝐝 :- [✨ 𝐂𝐔𝐓𝐄 🚩](https://t.me/invisible_avneesh)
-𝐅𝐞𝐞𝐋𝐢𝐧𝐠'𝐒 :- [✨ 𝗝𝗼𝗶𝗻 ❤️🥀](https://t.me/invisible_avneesh)
+𝐂𝐫𝐞𝐚𝐭𝐨𝐫 :- [✨ 𝐊𝐈𝐍𝐆💜](https://t.me/I_AM_KING_OP)
+𝐒𝐮𝐩𝐩𝐨𝐫𝐭 :- [✨ 𝐐𝐔𝐄𝐄𝐍 ❤️🎸](https://t.me/payalsahu1)
+𝐃𝐢𝐬𝐜𝐮𝐬𝐬 :- [✨ 𝐆𝐑𝐎𝐔𝐏 🎧](https://t.me/freinnd)
+𝐒𝐨𝐮𝐫𝐜𝐞  :- [✨ 𝐍𝐄𝐓𝐖𝐎𝐑𝐊 🌍](https://t.me/FROZY_BLOOD_BASHER)
+𝐂𝐨𝐦𝐦𝐚𝐧𝐝 :- [✨ 𝐂𝐔𝐓𝐄 🚩](https://t.me/I_AM_KING_OP)
+𝐅𝐞𝐞𝐋𝐢𝐧𝐠'𝐒 :- [✨ 𝗝𝗼𝗶𝗻 ❤️🥀](https://t.me/FROZY_BLOOD_BASHER)
 
-𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝗠𝗿 OWNER ❤️](https://t.me/classy_network)**""",
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [𝗠𝗿 OWNER ❤️](https://t.me/I_AM_KING_OP)**""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 Jᴏɪɴ Hᴇʀᴇ & Sᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/invisible_avneesh")
+                        "🥀 Jᴏɪɴ Hᴇʀᴇ & Sᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/freinnd")
                 ]
                 
            ]
@@ -36,13 +36,13 @@ async def start_(client: Client, message: Message):
 @Client.on_message(command(["repo"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/86aa550f64231113c8162.jpg",
+        photo=f"https://te.legra.ph/file/1cecce7c41d4f3fc5f0fc.jpg",
         caption=f"""""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "💥 GO TO MY GROUP  💞", url=f"https://t.me/secretgroupc")
+                        "💥 GO TO MY GROUP  💞", url=f"https://t.me/freinnd")
                 ]
             ]
         ),
